800d4ae25691bdb371647db271587a86159df367
Sarath Balivada <sarath.balivada95@gmail.com>
2023-02-23 17:55:41 +0530

    form update

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ const App = () => {
     watch,
     formState: { errors },
   } = useForm();
-    
-  const onSubmit = data => console.log(data);
+
+  const onSubmit = (data) => console.log(data);
+  console.log("errors", errors);
 
   return (
     <div className="container">
@@ -18,13 +19,53 @@ const App = () => {
         {/* form column */}
         <div className="column">
           <div className="left-half">
-            <h1>COntact Us</h1>
-            <p>We are open for suggestions</p>
+            <h1>Contact Us</h1>
+            <p>We are open for inputs and suggestions</p>
 
-            <form onSubmit={handleSubmit(onSubmit)} >
-                {/* Name */}
-                <p className="">                </p>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              {/* Name */}
+              <p className="input-label">Name *</p>
+              <input
+                className="input"
+                type="text"
+                placeholder="Name"
+                {...register("name", {
+                  required: true,
+                  maxLength: 80,
+                })}
+              />
+                 {errors.name && (
+                <span className="error">
+                  {errors.name.type === "required" &&
+                    "This field is manditory."}
+                  {errors.name.type === "maxLength" && "Less than 15 char"}
+                </span>
+              )}
 
+
+              {/* E-mail */}
+              <p className="input-label">Email *</p>
+              <input
+                className="input"
+                type="email"
+                placeholder="Email"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              />
+              {errors.email && (
+                <span className="error">
+                  {errors.email.type === "required" &&
+                    "This field is manditory."}
+                  {errors.email.type === "pattern" && "Invalid Email Address"}
+                </span>
+              )}
+
+              {/* Submit */}
+              <div>
+                <input className="submit" type="submit" value="send Message" />
+              </div>
             </form>
           </div>
         </div>
